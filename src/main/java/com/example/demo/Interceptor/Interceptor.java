94d80a1aaf30e2b29e74bba9c7bdb71520d0285e
HyunSangWon <hyunsangwon93@gmail.com>
2019-08-02 15:00:41 +0900

    admin site

---
+++ src/main/java/com/example/demo/Interceptor/Interceptor.java
@@ -0,0 +1,33 @@
+package com.example.demo.Interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+@Component
+public class Interceptor implements HandlerInterceptor{
+
+	private static final Logger logger = LoggerFactory.getLogger(Interceptor.class);
+	
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		
+		logger.debug("====================================================== PreHandle");
+		return true;
+	}
+	
+	
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+
+		logger.debug("====================================================== PostHandle");
+	}
+	
+}
